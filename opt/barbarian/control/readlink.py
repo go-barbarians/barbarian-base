f308ee32ec467de40d9b3acec26bfb2816ecc815
grobbie <rob@barbarians.io>
2018-10-04 08:17:03 +0200

    improve /usr/bin/readlink alike tool
    Signed-off-by: grobbie <rob@barbarians.io>

--- opt/barbarian/control/readlink.py
+++ opt/barbarian/control/readlink.py
@@ -1,13 +1,31 @@
 #!/opt/python27/bin/python
-import os, sys
+import os, sys, getopt
 
-def main():
-   path = sys.argv[1]
+def main(argv):
+   follow = False
+   path = ''
    try:
-      print os.path.join(os.path.dirname(os.readlink(path)), os.readlink(path))
+      opts, args = getopt.getopt(argv,'f:')
+   except getopt.GetoptError:
+      print 'readlink [-f] <link>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-f':
+         follow = True
+         path = arg
+      else:
+         follow = False
+   if path == '':
+      path = sys.argv[1]
+   try:
+      if follow:
+         print os.path.join(os.path.dirname(os.readlink(path)), os.readlink(path))
+      else:
+         print os.readlink(path)
       sys.exit()
    except OSError:
       print "invalid path"
       sys.exit(2)
+
 if __name__ == '__main__':
-    main()
+   main(sys.argv[1:])

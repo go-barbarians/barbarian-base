d4dbc3baf7e3fe95f83cbec8f4d8ec632f47a640
grobbie <rob@barbarians.io>
2018-10-06 11:28:13 +0200

    fix broken init script
    Signed-off-by: grobbie <rob@barbarians.io>

--- opt/barbarian/control/init.sh
+++ opt/barbarian/control/init.sh
@@ -7,11 +7,13 @@ then
 	GLIBC_REPO=https://github.com/sgerrand/alpine-pkg-glibc
 fi
 
+# Glibc as a whole is LGPL licensed. Some of its code is BSD/MIT/CMU/other licensed.
 if [[ -z "${GLIBC_VERSION}" ]]
 then
 	GLIBC_VERSION=2.28-r0
 fi
 
+# Java is GPL licensed with classpath exception
 if [[ -z "${JAVA8_DOWNLOAD}" ]]
 then
 	JAVA8_DOWNLOAD=https://github.com/ojdkbuild/contrib_jdk8u-ci/releases/download/jdk8u181-b13/jdk-8u181-ojdkbuild-linux-x64.zip
@@ -27,20 +29,41 @@ then
 	BASH_REPO=http://dl-cdn.alpinelinux.org/alpine/v3.3/main/x86_64
 fi
 
+# Bash is GPL licensed. 
+# You should be able to build programs that interoperate via
+# the stdin/stdout interface without being affected by the GPL, but
+# linking, reflecting/dynamically loading or otherwise invoking 
+# the GPL licensed code in an intimate way, for example within 
+# the same process, may trigger the copyleft aspect of the license 
+# conditions and require your program to also be GPL licensed,
+# or not distributed.
+# The prinicpal is that the program should always be able to
+# run without dependency on the GPL code. For example, if a future
+# version of the GPL code is released that is 10% faster than the
+# original version of the GPL code, then the user must be able to 
+# use *your* program with the new version of the GPL code without
+# modification.
+# https://www.gnu.org/licenses/gpl-faq.html
+#
 if [[ -z "${BASH_APK}" ]]
 then
 	BASH_APK=bash-4.3.42-r6.apk
 fi
 
-#if [[ -z "${BASH_NCURSES_APK}" ]]
-#then
-#	BASH_NCURSES_APK=ncurses-libs-6.0_p20170701-r0.apk
-#fi
-
+# Readline is a GPL licensed library.
+# Not LGPL.
 if [[ -z "${BASH_READLINE_APK}" ]]
 then
 	BASH_READLINE_APK=readline-6.3.008-r4.apk
 fi
+
+# Bash requires readline, which depends on ncursesw
+# we are currently shipping ncurses but not ncursesw
+if [[ -z "${BASH_NCURSES_APK}" ]]
+then
+	BASH_NCURSES_APK=ncurses-libs-6.0_p20170701-r0.apk
+fi
+
 
 PYTHON=/opt/python27/bin/python
 DOWNLOADER=/opt/barbarian/control/download.py
@@ -55,21 +78,25 @@ $PIP install urllib3
 
 for pkg in glibc-${GLIBC_VERSION} glibc-bin-${GLIBC_VERSION} glibc-i18n-${GLIBC_VERSION}
 do 
+	echo "downloading ${pkg}.apk"
 	$PYTHON $DOWNLOADER -u ${GLIBC_REPO}/releases/download/${GLIBC_VERSION}/${pkg}.apk -t /tmp/${pkg}.apk
 done
 
 #todo: MUST be a zip file currently, but the script *could* detect the filetype
-$PYTHON $DOWNLOADER -u $JAVA8_DOWNLOAD -t /tmp/jdk.zip
+echo "downloading ${JAVA_INSTALL_VERSION}.zip"
+$PYTHON $DOWNLOADER -u $JAVA8_DOWNLOAD -t /tmp/${JAVA_INSTALL_VERSION}.zip
 
 mkdir -p $JAVA_INSTALL_DIR
 
-$PYTHON $UNZIP -t /tmp/jdk.zip -o $JAVA_INSTALL_DIR
+echo "unpacking ${JAVA_INSTALL_VERSION}.zip"
+$PYTHON $UNZIP -t /tmp/${JAVA_INSTALL_VERSION}.zip -o $JAVA_INSTALL_DIR
 
 mkdir -p /opt/glibc
 
 cd /opt/glibc
 for pkg in glibc-${GLIBC_VERSION} glibc-bin-${GLIBC_VERSION} glibc-i18n-${GLIBC_VERSION}
 do 
+	echo "unpacking ${pkg}.apk"
 	$PYTHON $UNTAR /tmp/${pkg}.apk
 done
 cd /tmp
@@ -91,14 +118,25 @@ HOSTNAME=$(cat /etc/hostname)
 echo "127.0.0.1 localhost ${HOSTNAME}" > /etc/hosts
 echo 'hosts: files mdns4_minimal [NOTFOUND=return] dns mdns4' >> /etc/nsswitch.conf
 
+echo "downloading ${BASH_APK}"
 python /opt/barbarian/control/download.py -u $BASH_REPO/$BASH_APK -t /tmp/bash.apk
+
+echo "downloading ${BASH_NCURSES_APK}"
 python /opt/barbarian/control/download.py -u $BASH_REPO/$BASH_NCURSES_APK -t /tmp/ncurses.apk
+
+echo "downloading ${BASH_READLINE_APK}"
 python /opt/barbarian/control/download.py -u $BASH_REPO/$BASH_READLINE_APK -t /tmp/readline.apk
 
 # this path is prebaked, along with a symlink to /bin/bash and LD_LIBRARY_PATH env var.
 cd /opt/bash
+echo "unpacking $BASH_APK"
 python /opt/barbarian/control/untar.py /tmp/bash.apk
+
+echo "unpacking $BASH_NCURSES_APK"
 python /opt/barbarian/control/untar.py /tmp/ncurses.apk
+
+echo "unpacking $BASH_READLINE_APK"
 python /opt/barbarian/control/untar.py /tmp/readline.apk
 
 cd /tmp
+
